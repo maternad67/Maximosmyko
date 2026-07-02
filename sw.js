3ef55e8871dc3effd8fdfe3a09d1ada58cfbb551
David <davidak0456@gmail.com>
2026-07-03 00:31:34 +0200

    Update service worker cache version and management
    Updated cache version and added cache management for old versions.

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'maximosmyko-v1';
+// PŘI KAŽDÉ DALŠÍ ZMĚNĚ WEBU ZVEDNI TOTO ČÍSLO (v2, v3, v4...)
+const CACHE_NAME = 'maximosmyko-v2'; 
+
 const urlsToCache = [
   './',
   './index.html',
@@ -8,13 +10,31 @@ const urlsToCache = [
   './vnitrekmaximo.jpg'
 ];
 
+// Instalace a uložení do paměti
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Vnutí okamžitou instalaci nové verze
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Smazání staré paměti při vydání nové verze
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Smaže staré verze (v1)
+          }
+        })
+      );
+    })
+  );
+});
+
+// Načítání souborů
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
